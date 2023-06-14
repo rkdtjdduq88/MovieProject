7c174ac866a569d1dba7021e1f477feaea86ef47
rkdtjdduq88 <rkdtjdduq88@naver.com>
2023-06-14 14:39:36 +0900

    project basic

---
+++ src/main/java/com/project/movie/BootMovieApplication.java
@@ -0,0 +1,13 @@
+package com.project.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMovieApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/project/movie/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.project.movie.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String main() {
+		log.info("main 폼 요청");
+		return "main";
+	}
+}
