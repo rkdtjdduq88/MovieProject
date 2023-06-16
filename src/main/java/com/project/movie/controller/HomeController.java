24cd08053395256e764d64ad1d9a42d729b3c9dc
Sue <roonhyeon@gmail.com>
2023-06-16 12:16:08 +0900

    연습

--- src/main/java/com/project/movie/controller/HomeController.java
+++ src/main/java/com/project/movie/controller/HomeController.java
@@ -12,10 +12,6 @@ public class HomeController {
 	@GetMapping("/")
 	public String main() {
 		log.info("main 폼 요청");
-<<<<<<< HEAD
-		return "index";
-=======
 		return "boxoffice";
->>>>>>> refs/heads/seungoh
 	}
 }
